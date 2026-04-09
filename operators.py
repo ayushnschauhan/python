5452ecca57ce404c9d198b223866191097983135
ayush chauhan <ayushchauhan123498@gmail.com>
2026-04-09 16:47:57 +0530

    Add files via upload

---
+++ operators.py
@@ -0,0 +1,19 @@
+# ARITHMETIC OPERATORS
+a = 45
+b = 45 
+c = a + b # addition
+
+print(c)
+
+# ASSIHNMENT OPERATORS  
+
+a = 4-2 # Assign 4-2 in a
+print(a)
+b = 6
+b += 3 # INCREMENT THE VALUE OF B BY 3 AND THE ASSIGN IT TO A B
+print(b)
+
+# comparison operators
+
+d = 5>4
+print(d)
---
+++ variables.py
@@ -0,0 +1,6 @@
+a = 1
+b = 2
+
+print(a+b)
+
+# variables are the continers that stores the values
